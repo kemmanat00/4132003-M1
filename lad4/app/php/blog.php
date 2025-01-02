0406e86761cb1291fd799236e5b89191b9f000d9
kemmanat00 <kemmanatnakinchat@gmail.com>
2025-01-02 11:16:02 +0700

    lad4

--- lad4/app/php/blog.php
+++ lad4/app/php/blog.php
@@ -6,6 +6,39 @@ $method = $_SERVER['REQUEST_METHOD'];
 $response = ['status' => 'error', 'message' => 'Invalid request'];
 
 switch ($method) {
+    case 'GET'
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            $stmt = $conn->prepare("SELECT * FROM tb_blog WHERE id = ?");
+            $stmt ->bind_param("i", $id);
+            $result = $stmt->get_result();
+            $blog = $result->fetch_assoc();
+
+            if($blog){
+                $response = ['status'=>'success','data'=> $blog];
+            }else{
+                $response =['status'=>'error','message'=>'blog not found'];
+            }
+        } else{
+            //get all
+            $stmt = $conn->prepare("SELECT * FROM tb_blog");
+            $stmt ->execute();
+            $result = $stmt ->get_result();
+
+            while($row = $stmt->get_result()){
+                $blog[] = $row;
+            }
+            $response = ['status'=>'success','data' => $blog];
+        }
+        break;
+
+
+
+
+
+
+
+
     case 'POST': // Insert new blog
         $title = $_POST['title'] ?? null;
         $post = $_POST['post'] ?? null;
